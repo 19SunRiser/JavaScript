750d7a91419c56b66b0a6d8efc0a36efef2e458d
isMack <87532705+19SunRiser@users.noreply.github.com>
2024-03-26 04:22:26 +0000

    ResvisionClass

---
+++ Resvision/allDataType.js
@@ -0,0 +1,18 @@
+console.log("hellow world")
+
+let s = Symbol('123');
+let s2 = null;
+console.log(typeof s);
+
+let st = "hellow mamu"
+const obj = {
+    name:'rahul',
+    age:22
+}
+console.log(obj);
+
+
+for (let index = 1; index < 10; index++) {
+        console.log(index)
+    
+}
